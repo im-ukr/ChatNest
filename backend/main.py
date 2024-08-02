6b1d15923b2d7a83a87638ebd37f565b02136740
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-08-03 00:26:32 +0530

    project added

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+PRIVATE_KEY="138226ef-5121-429d-9dc0-1886b6b13196"
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Private-Key": PRIVATE_KEY }
+    )
+    return response.json()
+
---
+++ js-chat-website/README.md
@@ -0,0 +1,26 @@
+# ChatNest - Live Chat Experience with Simplified UI💬 
+
+This project is a chat user interface developed using JavaScript, specifically with React.
+
+To get started with the ChatNest application, ensure that NPM (Node Package Manager) is installed on your system.
+
+To run the website, clone the director, `cd` into it with your terminal:
+
+## 1. Add `.env` file
+
+Within your `.env` file, add the following with your [ChatEngine.io](ChatEngine.io) Project ID
+
+```
+VITE_CHAT_ENGINE_PROJECT_ID=XXXX
+```
+
+## 2. Run the code!
+
+```
+npm install # to install all packages
+npm run dev # to start the website
+```
+
+Then, your website will be running at http://localhost:5173/
+
+Done 🎉
